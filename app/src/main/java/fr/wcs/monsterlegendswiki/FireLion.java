08bc358f374b8d6854ad7bd264b91c3a72b6848e
WOLFF Benjamin <wolfbenjamin31@gmail.com>
2018-03-07 16:24:38 +0100

    ajout seek bar et premier design

--- app/src/main/java/fr/wcs/monsterlegendswiki/FireLion.java
+++ app/src/main/java/fr/wcs/monsterlegendswiki/FireLion.java
@@ -5,16 +5,79 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class FireLion extends AppCompatActivity {
 
     private ImageView retourFire;
+    TextView textViewTitle;
+    TextView textViewPower;
+    TextView textViewLife;
+    TextView textViewSpeed;
+    TextView textViewStamina;
+    TextView textViewValue;
+    SeekBar seekBarLevel;
+    ImageView imageViewMonster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_lion);
+
+        textViewTitle = findViewById(R.id.textView_Firelion);
+        textViewPower = findViewById(R.id.textView_power);
+        textViewLife = findViewById(R.id.textView_life);
+        textViewSpeed = findViewById(R.id.textView_speed);
+        textViewStamina = findViewById(R.id.textView_stamina);
+        seekBarLevel = findViewById(R.id.seekBar_level);
+        textViewValue = findViewById(R.id.textView_value);
+        imageViewMonster = findViewById(R.id.imageView_monster);
+
+        seekBarLevel.setMax(20);
+
+        final MyMonster monsterFirelion = new MyMonster(1, 242, 81, 192, 100);
+
+        fillTextView(monsterFirelion);
+
+        seekBarLevel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int level = i;
+                if (level == 0) {
+                    level = 1;
+                }
+                textViewValue.setText(String.valueOf(level));
+                monsterFirelion.evolution(level);
+                fillTextView(monsterFirelion);
+                if (level < 4){
+                    imageViewMonster.setImageResource(R.drawable.firelion);
+                }
+                else if (level < 7){
+                    imageViewMonster.setImageResource(R.drawable.ui_fire_lion_2);
+                }
+                else {
+                    imageViewMonster.setImageResource(R.drawable.ui_fire_lion_3);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+
+
 
         this.retourFire = findViewById(R.id.retour_fire);
 
@@ -26,6 +89,14 @@ public class FireLion extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+    public void fillTextView(MyMonster firelion){
+        textViewTitle.setText("Fire Lion level : " + firelion.getmLevel());
+        textViewLife.setText("Life : " + firelion.getmLife());
+        textViewSpeed.setText("Speed : " + firelion.getmSpeed());
+        textViewStamina.setText("Stamina : " + firelion.getmStamina());
+        textViewPower.setText("Power : "   + firelion.getmPower());
     }
 }
 
---
+++ app/src/main/java/fr/wcs/monsterlegendswiki/SlashScreen.java
@@ -0,0 +1,8 @@
+package fr.wcs.monsterlegendswiki;
+
+/**
+ * Created by wilder on 07/03/18.
+ */
+
+public class SlashScreen {
+}
