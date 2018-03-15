ede28eb443f6a5abc387e24f39fe58b73c7c18cb
WOLFF Benjamin <wolfbenjamin31@gmail.com>
2018-03-15 10:42:09 +0100

    maj apie

---
+++ app/src/main/java/fr/wcs/monsterlegendswiki/PayeActivity.java
@@ -0,0 +1,13 @@
+package fr.wcs.monsterlegendswiki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paye);
+    }
+}
